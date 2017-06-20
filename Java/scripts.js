51ce8eb418d52628bb200ca4b5da152e05115ed5
Benji96 <benji_al96@hotmail.com>
2017-06-20 21:14:19 +0200

    Update scripts.js

--- Java/scripts.js
+++ Java/scripts.js
@@ -47,7 +47,7 @@ setTimeout(function(){
     	bscenes.style.animationName = "inright";
    	bprogramming.style.animationName = "inright";
     
-	 }, 2500);
+	 }, 1000);
 
 bmodeling.onclick = function () {
 	
